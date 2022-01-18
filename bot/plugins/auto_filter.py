dfdf1772933bf4f4000e65019a9fbd2c0986bd09
navaneethrkrishna <95025235+navaneethrkrishna@users.noreply.github.com>
2022-01-18 15:16:52 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} This Is Your Request File @malayalamcinemaas: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
